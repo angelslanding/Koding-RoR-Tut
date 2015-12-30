b270e9ef54d62ee991528a8c4eee5be13240f05e
Koding-RoR-Tut <angelsown4056@yahoo.com>
2015-12-30 23:39:21 +0000

    create new template, action and route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,19 @@
 class ArticlesController < ApplicationController
+    def new 
+        @article = Article.new
+    end
+    def create 
+        #render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to articles_show(@article)
+    end 
+    
+    private 
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end    
+=begin    
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
@@ -71,4 +86,5 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
+=end    
 end
